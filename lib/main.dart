7d5d6d8cac98ccbfd28dbadda55135de4ca42453
Andrei Pranko <djmentarey@gmail.com>
2019-01-28 16:43:04 +0300

    some fixes

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
       home: SearchScreen(title: 'Поиск книг'),
     );
@@ -79,8 +70,7 @@ class _SearchScreenState extends State<SearchScreen> {
 
   void _onNext(dynamic book) {
     setState(() {
-      _items.add(Book(book["title"],
-          book["imageLinks"]["thumbnail"]));
+      _items.add(Book(book["title"], book["imageLinks"]["thumbnail"]));
     });
   }
 
@@ -105,11 +95,9 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: Container(
-        padding: EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(8.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
@@ -123,7 +111,7 @@ class _SearchScreenState extends State<SearchScreen> {
               onChanged: (string) => subject.add(string),
             ),
             Container(
-              padding: EdgeInsets.all(16.0),
+              padding: EdgeInsets.all(8.0),
               child: _isLoading ? CircularProgressIndicator() : Container(),
             ),
             Flexible(
